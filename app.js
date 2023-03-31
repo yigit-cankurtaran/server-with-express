ff78ac487db0b8489497235454e515f33162a5b3
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-31 14:01:53 +0300

    Made it display 'Hello World'

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const http = require("http");
 
-const server = http.createServer();
+const server = http.createServer((request, response) => {
+  response.end("Hello World");
+});
 server.listen(3000);
 
 // loading indefinitely. means it's working.
