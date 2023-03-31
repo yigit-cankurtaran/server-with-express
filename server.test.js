07ee98599e88e626e598b4a22729e826bf4df6d3
Yiğit Cankurtaran <yigitcankurtaranyc@gmail.com>
2023-03-31 14:48:38 +0300

    Wrote a small unit test for it.

--- server.test.js
+++ server.test.js
@@ -1,3 +1,28 @@
-// write a unit test for the server.js file
+const http = require("http");
+const app = require("./app");
 
-// Path: server.js
+const server = http.createServer(app);
+
+server.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+server.on("request", (request, response) => {
+  console.log("Request received");
+  if (request.method === "GET" && request.url === "/") {
+    response.statusCode = 200;
+    console.log("Hello World");
+  }
+});
+
+http.get("http://localhost:3000", (response) => {
+  if (response.statusCode === 200) {
+    console.log("Server runs properly");
+  } else {
+    console.log("Server has a problem");
+  }
+});
+
+server.on("close", () => {
+  console.log("Server is closed");
+});
